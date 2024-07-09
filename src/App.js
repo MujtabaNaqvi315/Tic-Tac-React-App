23117612111d93855b5e40ae12cb58186632e711
Syed Muhammad Mujtaba Naqvi <smmujtabanaqvi315@gmail.com>
2024-07-09 01:40:23 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-function Square({ value }) {
+import { useState } from "react";
+
+function Square() {
+  const [value, setvalue] = useState(null);
 
   function handleClick (){
-    console.log("Clicked!");
+    setvalue('X');
   }
 
   return (
@@ -15,19 +18,19 @@ export default function Board() {
   return(
    <>
     <div className="board-row">
-      <Square value="1" />
-      <Square value="2" />
-      <Square value="3" />
+      <Square />
+      <Square />
+      <Square />
     </div>
     <div className="board-row">
-      <Square value="4" />
-      <Square value="5" />
-      <Square value="6" />
+      <Square />
+      <Square />
+      <Square />
     </div>
     <div className="board-row">
-      <Square value="7" />
-      <Square value="8" />
-      <Square value="9" />
+      <Square />
+      <Square />
+      <Square />
     </div>
    </> 
   ); 
